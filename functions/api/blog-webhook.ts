6ee3f6ee8a7451da7e161da215aa0f121ee4998f
Tom Sherman <the.tomsherman@gmail.com>
2022-11-19 14:51:28 +0000

    WIP

--- functions/api/blog-webhook.ts
+++ functions/api/blog-webhook.ts
@@ -1,7 +1,61 @@
-export const onRequest: PagesFunction = async ({ request }) => {
-  console.log("Incoming webook");
+import { z } from "zod";
+
+export const onRequest: PagesFunction<{
+  BLOG_WEBHOOK_SECRET: string;
+}> = async ({ request, env }) => {
+  if (request.method !== "post") {
+    return new Response("Method not allowed", { status: 405 });
+  }
+
+  const signature = request.headers.get("x-hub-signature-256");
+  if (!signature) {
+    return new Response("Missing signature", { status: 400 });
+  }
+
+  const encoder = new TextEncoder();
+  if (!signature) {
+    return new Response("Missing signature", { status: 400 });
+  }
+  const body = await request.clone().text();
+
+  const verified = await crypto.subtle.verify(
+    "NODE-ED25519",
+    await crypto.subtle.importKey(
+      "raw",
+      encoder.encode(env.BLOG_WEBHOOK_SECRET),
+      { name: "SHA-256", hash: { name: "SHA-256" } },
+      true,
+      ["verify"]
+    ),
+    encoder.encode(signature),
+    encoder.encode(body)
+  );
+
+  if (!verified) {
+    return new Response("Not authorised", { status: 403 });
+  }
+
+  const parseResult = pushEventSchema.safeParse(JSON.parse(body));
+  if (!parseResult.success) {
+    return new Response("Invalid payload", { status: 400 });
+  }
+  const event = parseResult.data;
+
+  if (signature) console.log("Incoming webook");
   console.log(request.url);
-  console.log(await request.text());
+  console.log(event);
 
   return new Response("OK", { status: 200 });
 };
+
+const pushEventSchema = z.object({
+  ref: z.string(),
+  commits: z.array(
+    z.object({
+      id: z.string(),
+      added: z.array(z.string()),
+      removed: z.array(z.string()),
+      modified: z.array(z.string()),
+    })
+  ),
+});
