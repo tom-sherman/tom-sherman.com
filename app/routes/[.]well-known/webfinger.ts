4839bba844fca59bbf86c1b5dec831cecd17fa77
Tom Sherman <the.tomsherman@gmail.com>
2022-12-20 16:58:22 +0000

    Add webfinger

---
+++ app/routes/[.]well-known/webfinger.ts
@@ -0,0 +1,49 @@
+import { LoaderArgs } from "@remix-run/cloudflare";
+
+export function loader({ request }: LoaderArgs) {
+  const url = new URL(request.url);
+  const resourceQuery = url.searchParams.get("resource");
+
+  if (!resourceQuery) {
+    return new Response("Missing resource query parameter", {
+      status: 400,
+    });
+  }
+
+  if (resourceQuery !== "acct:me@tom-sherman.com") {
+    return new Response("Not found", {
+      status: 404,
+    });
+  }
+
+  return new Response(
+    JSON.stringify({
+      subject: "acct:tomsherman@fosstodon.org",
+      aliases: [
+        "https://fosstodon.org/@tomsherman",
+        "https://fosstodon.org/users/tomsherman",
+      ],
+      links: [
+        {
+          rel: "http://webfinger.net/rel/profile-page",
+          type: "text/html",
+          href: "https://fosstodon.org/@tomsherman",
+        },
+        {
+          rel: "self",
+          type: "application/activity+json",
+          href: "https://fosstodon.org/users/tomsherman",
+        },
+        {
+          rel: "http://ostatus.org/schema/1.0/subscribe",
+          template: "https://fosstodon.org/authorize_interaction?uri={uri}",
+        },
+      ],
+    }),
+    {
+      headers: {
+        "content-type": "application/jrd+json",
+      },
+    }
+  );
+}
