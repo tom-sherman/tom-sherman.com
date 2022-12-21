0ee8315ca02ec9315a126c618721a947597c38d8
Tom Sherman <the.tomsherman@gmail.com>
2022-12-21 12:25:54 +0000

    Add content type (#15)

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,8 @@
-import type { LinksFunction, MetaFunction } from "@remix-run/cloudflare";
+import type {
+  HeadersFunction,
+  LinksFunction,
+  MetaFunction,
+} from "@remix-run/cloudflare";
 import {
   Links,
   LiveReload,
