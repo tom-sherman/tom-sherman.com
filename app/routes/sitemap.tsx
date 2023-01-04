70333aee624cd4ff712709dec6845c71c6cdcc81
Tom Sherman <the.tomsherman@gmail.com>
2023-01-04 10:07:33 +0000

    Add plaintext sitemap

---
+++ app/routes/sitemap.tsx
@@ -0,0 +1,33 @@
+import type { LoaderArgs } from "@remix-run/cloudflare";
+import { createD1Kysely, D1BlogData } from "~/lib/blog-data.server";
+import { HOST } from "~/constants";
+
+export async function loader({ context }: LoaderArgs) {
+  const blog = new D1BlogData(createD1Kysely((context as any).env.DB));
+
+  const [posts, tags] = await Promise.all([
+    blog.listAllPosts(),
+    blog.listAllTags(),
+  ]);
+
+  const postUrls = posts.map((post) =>
+    new URL(`/blog/${post.slug}`, HOST).toString()
+  );
+
+  const tagUrls = tags.map((tag) =>
+    new URL(`/blog/tags/${tag}`, HOST).toString()
+  );
+
+  const allUrls = [
+    new URL("/", HOST).toString(),
+    new URL("/blog", HOST).toString(),
+    ...postUrls,
+    ...tagUrls,
+  ];
+
+  return new Response(allUrls.join("\n"), {
+    headers: {
+      "Content-Type": "text/plain",
+    },
+  });
+}
