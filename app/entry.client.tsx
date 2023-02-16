4dcf064131fdae6630c13d20cbe79726a42f253a
Tom Sherman <the.tomsherman@gmail.com>
2023-02-16 23:26:23 +0000

    Non blocking hydration
    See the following tweets for wtf this is about:
    
    - https://twitter.com/ryanflorence/status/1547959632663961602
    - https://twitter.com/dan_abramov/status/1567852606642348032
    
    Basically wrapping hydrateRoot in startTransition ensures that hydration
    is timesliced.

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,12 @@
 import { RemixBrowser } from "@remix-run/react";
+import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 
-hydrateRoot(document, <RemixBrowser />);
+startTransition(() => {
+  hydrateRoot(
+    document,
+    <StrictMode>
+      <RemixBrowser />
+    </StrictMode>
+  );
+});
